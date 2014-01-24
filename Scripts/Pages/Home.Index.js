4140a08d5dd29e2ccfcb4effe602eafa456c0173
Matthew Nichols <matthew@matthew-nichols.com>
2014-01-24 16:32:43 -0700

    Initial Commit.

---
+++ Scripts/Pages/Home.Index.js
@@ -0,0 +1,10 @@
+﻿$(function() {
+
+    $('.details').highcharts({
+
+    });
+
+
+});
+
+//http://jsfiddle.net/gh/get/jquery/1.9.1/highslide-software/highcharts.com/tree/master/samples/highcharts/demo/line-basic/
